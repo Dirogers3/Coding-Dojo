452dc30d6d06941f65bb30858e007e1b0d8152d3
Daniel Rogers <danielirogers3@gmail.com>
2021-04-21 16:19:50 -0700

    Created and updated Video Preview

---
+++ Dojo_Assignments/WebFundamentals/Javascript/Video_Preview/script.js
@@ -0,0 +1,9 @@
+function over() {
+    console.log("Test");
+    document.getElementById("myvideo").play();
+    // element.play();
+}
+function out() {
+    // element.pause();
+    document.getElementById("myvideo").pause();
+}
