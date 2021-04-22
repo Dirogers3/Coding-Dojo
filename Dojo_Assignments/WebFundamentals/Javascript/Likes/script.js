1d21f5e66f7bda0b5f77fa984bd3c1ffb172b051
Ian <danielirogers3@gmail.com>
2021-04-22 13:00:19 -0700

    update to likes

--- Dojo_Assignments/WebFundamentals/Javascript/Likes/script.js
+++ Dojo_Assignments/WebFundamentals/Javascript/Likes/script.js
@@ -1,16 +1,11 @@
-var countlikes = document.querySelector("#count");
-var count = 0;
-function like() {
-    count++;
-    countlikes.innerHTML = count + " like(s) "
-}
-var count2 = 0;
-function like2() {
-    count2++;
-    document.getElementById("count2").innerHTML= count2 + " like(s)";
-}
-var count3 = 0;
-function like3() {
-    count3++;
-    document.getElementById("count3").innerHTML= count3 + " like(s)";
+
+var count = {
+    1:0,
+    2:0,
+    3:0
+};
+function like(element) {
+    var countlikes = document.querySelector("#count" + element);
+    count[element]++;
+    countlikes.innerHTML = count[element] + " like(s) "
 }
