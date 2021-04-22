6de3139eb45cb0e0ea731dbb1bd3ada8465d09dd
Daniel Rogers <danielirogers3@gmail.com>
2021-04-22 10:10:08 -0700

    Update to many things

--- Dojo_Assignments/WebFundamentals/Javascript/Likes part 1/script.js
+++ Dojo_Assignments/WebFundamentals/Javascript/Likes part 1/script.js
@@ -3,3 +3,13 @@ function like() {
     count++;
     document.getElementById("count").innerHTML= count + " like(s)";
 }
+var count2 = 0;
+function like2() {
+    count2++;
+    document.getElementById("count2").innerHTML= count2 + " like(s)";
+}
+var count3 = 0;
+function like3() {
+    count3++;
+    document.getElementById("count3").innerHTML= count3 + " like(s)";
+}
