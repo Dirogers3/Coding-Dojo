21fa3ecf2c528e8dacb593481c2d7609025a5141
dirogers <rog17006@byui.edu>
2021-05-03 14:57:00 -0700

    created Flask Hello

---
+++ Dojo_Assignments/python_/flask/fundamentals/hello_flask/hello.py
@@ -0,0 +1,38 @@
+from flask import Flask  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+@app.route('/success')
+def success():
+    return "success"
+@app.route('/say/<name>')
+def say(name):
+    print(name)
+    return "Hi, " + name
+@app.route('/repeat/<number>/<word>')
+def repeat(number, word):
+    sentence=""
+    for i in range(int(number)):
+        sentence += (word +"\n")
+    return sentence
+    
+@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hello(name):
+    print(name)
+    return "Hello, " + name
+@app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id: " + id
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
+
+
+
