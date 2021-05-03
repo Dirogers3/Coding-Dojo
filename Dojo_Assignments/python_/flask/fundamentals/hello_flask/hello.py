b54eefb3bdacdee8bc51cf29341142546160e949
dirogers <rog17006@byui.edu>
2021-05-03 15:17:47 -0700

    Update

--- Dojo_Assignments/python_/flask/fundamentals/hello_flask/hello.py
+++ Dojo_Assignments/python_/flask/fundamentals/hello_flask/hello.py
@@ -3,33 +3,41 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
+
 @app.route('/dojo')
 def dojo():
     return "Dojo!"
+
 @app.route('/success')
 def success():
     return "success"
+
 @app.route('/say/<name>')
 def say(name):
     print(name)
-    return "Hi, " + name
+    return ("Hi, " + str(name))
+
 @app.route('/repeat/<number>/<word>')
 def repeat(number, word):
     sentence=""
     for i in range(int(number)):
         sentence += (word +"\n")
-    return sentence
+    return str(sentence)
     
 @app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
 def hello(name):
     print(name)
     return "Hello, " + name
+
 @app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
 def show_user_profile(username, id):
     print(username)
     print(id)
     return "username: " + username + ", id: " + id
 
+@app.route('/<something>')
+def function(something):
+    return "Sorry! No response. Try again."
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
