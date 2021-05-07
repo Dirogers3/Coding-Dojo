ff7c654e36bca9a704083a6da18da7b4a8806ce7
Ian <danielirogers3@gmail.com>
2021-05-07 15:26:16 -0700

    Update to Great Number Game

---
+++ Dojo_Assignments/python_/flask/fundamentals/Great_Number_game/great_number_game.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+app = Flask(__name__)
+app.secret_key ='This is a secret'
+
+@app.route('/')
+def game():
+    if 'random-number' in session:
+        session.pop('random-number')
+        session['random-number'] = random.randint(1,100)
+    else: 
+        session['random-number'] = random.randint(1,100)
+
+    if 'tries' in session:
+        session.pop('tries')
+        session['tries'] = 0
+    else:
+        session['tries'] = 0
+    return render_template("index.html")
+
+@app.route('/guess/', methods=['POST'])
+def guess():
+    random_number = session['random-number']
+    form_guess = request.form["guess"]
+    print(session)
+    session['tries'] += 1
+    form_tries = session['tries']
+    return render_template("results.html", randomNum = int(random_number), guessNumber = int(form_guess), template_tries = int(form_tries))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
