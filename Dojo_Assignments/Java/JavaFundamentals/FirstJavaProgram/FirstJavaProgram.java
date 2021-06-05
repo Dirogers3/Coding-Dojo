08b23d20670251d082169df92ac18b3743ed5777
Ian <danielirogers3@gmail.com>
2021-06-05 00:01:56 -0700

    Created First Java Program

---
+++ Dojo_Assignments/Java/JavaFundamentals/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,11 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        String name= "Ian";
+        int age = 25;
+        String hometown = "San Diego, CA";
+        System.out.println("My name is "+name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + hometown);
+
+    }
+}
