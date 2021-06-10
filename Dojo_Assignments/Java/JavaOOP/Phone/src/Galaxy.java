ca041d35491215b87be2b3558da4ffb5e94854d1
Ian <danielirogers3@gmail.com>
2021-06-10 12:54:19 -0700

    Created Phone and finished

---
+++ Dojo_Assignments/Java/JavaOOP/Phone/src/Galaxy.java
@@ -0,0 +1,21 @@
+
+public class Galaxy extends Phone implements Ringable{
+	public Galaxy(String versionNumber, int batteryPercentage, String carrier, String ringTone) {
+        super(versionNumber, batteryPercentage, carrier, ringTone);
+    }
+	@Override
+    public String ring() {
+       return "Galaxy " + getVersionNumber() + " Says "+ getRingTone();
+    }
+    @Override
+    public String unlock() {
+        // your code here
+    	return "Unlocking via finger print";
+    }
+    @Override
+    public void displayInfo() {
+        // your code here  
+        System.out.println("Galaxy " + getVersionNumber() + " from " + getCarrier());
+
+    }
+}
---
+++ Dojo_Assignments/Java/JavaOOP/Phone/src/IPhone.java
@@ -0,0 +1,21 @@
+
+public class IPhone extends Phone implements Ringable{
+	public IPhone(String versionNumber, int batteryPercentage, String carrier, String ringTone) {
+        super(versionNumber, batteryPercentage, carrier, ringTone);
+    }
+	@Override
+    public String ring() {
+       return "IPhone " + getVersionNumber() + " Says "+ getRingTone();
+    }
+    @Override
+    public String unlock() {
+        // your code here
+    	return "Unlocking via facial recognition";
+    }
+    @Override
+    public void displayInfo() {
+        // your code here  
+        System.out.println("iPhone " + getVersionNumber() + " from " + getCarrier());
+
+    }
+}
