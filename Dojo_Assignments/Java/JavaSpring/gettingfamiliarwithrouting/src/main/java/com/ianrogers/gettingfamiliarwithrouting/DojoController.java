f09d19a53aa8346a79470b6e98a4cd32bf347fd7
dirogers <rog17006@byui.edu>
2021-06-11 14:40:50 -0700

    Created Getting Familiar with Routing

---
+++ Dojo_Assignments/Java/JavaSpring/gettingfamiliarwithrouting/src/main/java/com/ianrogers/gettingfamiliarwithrouting/DojoController.java
@@ -0,0 +1,18 @@
+package com.ianrogers.gettingfamiliarwithrouting;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DojoController {
+	@RequestMapping("/{x}")
+	public String dojo(@PathVariable("x") String x) {
+		String output = "The " + x + " is awesome!";
+		if(x.equals("burbank-dojo")) {
+			output = "Burbank Dojo is located in Southern California";
+		}
+		return output;
+	}
+
+}
