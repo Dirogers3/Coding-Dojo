b57f35a7bfff7ad96296fde61368da62ee0b54c1
dirogers <rog17006@byui.edu>
2021-06-11 13:43:33 -0700

    Created Strings and Finished

--- Dojo_Assignments/Java/JavaOOP/SinglyLinkedList/SinglyLinkedList.java
+++ Dojo_Assignments/Java/JavaOOP/SinglyLinkedList/SinglyLinkedList.java
@@ -11,15 +11,15 @@ public class SinglyLinkedList {
     // SLL methods go here. As a starter, we will show you how to add a node to the
     // list.
     public void add(int value) {
-        Node newNode = new Node(value);
+        Node stickyNote= new Node(value);
         if (head == null) {
-            head = newNode;
+            head = stickyNote;
         } else {
-            Node runner = head;
-            while (runner.next != null) {
-                runner = runner.next;
+            Node pointer = head;
+            while (pointer.next != null) {
+                pointer = pointer.next;
             }
-            runner.next = newNode;
+            pointer.next = stickyNote;
         }
     }
 
@@ -35,8 +35,6 @@ public class SinglyLinkedList {
             }
             
             currentNode.next = null;
-            
-
         }
     }
 
