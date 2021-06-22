04dd0b773f5e4b243de22cb656a6154d386c86ec
Ian <danielirogers3@gmail.com>
2021-06-22 15:57:08 -0700

    Updated and Delete

--- Dojo_Assignments/Java/JavaSpring/Books/src/main/java/com/ianrogers/books/controllers/BooksApi.java
+++ Dojo_Assignments/Java/JavaSpring/Books/src/main/java/com/ianrogers/books/controllers/BooksApi.java
@@ -13,7 +13,7 @@ import com.ianrogers.books.services.BookService;
 
 @RestController
 public class BooksApi {
-	private final BookService bookService;
+private final BookService bookService;
 	
 	public BooksApi(BookService bookService) {
 		this.bookService = bookService;
@@ -24,19 +24,8 @@ public class BooksApi {
 		return bookService.allBooks();
 	}
 	
-	@RequestMapping(value="/api/books", method=RequestMethod.POST)
-    public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
-        Book book = new Book(title, desc, lang, numOfPages);
-        return bookService.createBook(book);
-    }
     
-    @RequestMapping("/api/books/{id}")
-    public Book show(@PathVariable("id") Long id) {
-        Book book = bookService.findBook(id);
-        return book;
-    }
-    
-    @RequestMapping(value="/api/books/{id}", method=RequestMethod.PUT)
+	@RequestMapping(value="/api/books/{id}", method=RequestMethod.PUT)
     public Book update(@PathVariable("id") Long id, @RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
         Book book = bookService.updateBook(id, title, desc, lang, numOfPages);
         return book;
