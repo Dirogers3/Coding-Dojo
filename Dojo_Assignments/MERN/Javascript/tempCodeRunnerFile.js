93eb4f060787a87fdbdf02898d8df7af4f25be0f
dirogers <rog17006@byui.edu>
2021-07-06 11:59:05 -0700

    Created and finished How long did that take?

--- Dojo_Assignments/MERN/Javascript/tempCodeRunnerFile.js
+++ Dojo_Assignments/MERN/Javascript/tempCodeRunnerFile.js
@@ -1,10 +1,20 @@
-const person = {
-    name: 'Phil Smith',
-    age: 47,
-    height: '6 feet'
-}
-const password = '12345';
-const { password: hashedPassword } = person;
-//Predict the output
-console.log(password); // 12345
-//console.log(hashedPassword); // nothing, there is not a hashedpassword in person.
+Number.prototype.isPrime = function() {
+//     for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+//         if(num % i === 0) return false; 
+//     return num > 1;
+// }
+
+// const { performance } = require('perf_hooks');
+// const start = performance.now();
+// let primeCount = 0;
+// let num = 2; // for math reasons, 1 is considered prime
+// while( primeCount < 1e6 ) {
+//     if( num.isPrime() ) {
+//         primeCount++;
+//     }
+//     num++;
+// }
+// console.log(`The 10,000th prime number is ${num-1}`);
+// console.log(`This took ${performance.now() - start} milliseconds to run`);
+
+// // for fibonacci numbers iterative will be faster when there are more numbers and recursive is faster when the numbers are small.
